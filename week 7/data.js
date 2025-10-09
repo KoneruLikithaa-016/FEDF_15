123f708a95697bb7acb66999b63d2be23188cd54
KoneruLikithaa-016 <2420030411@klh.edu.in>
2025-10-09 10:51:55 +0530

    Add files via upload

---
+++ week 7/data.js
@@ -0,0 +1,7 @@
+export const books = [
+    { title: "JavaScript Essentials", author: "John Doe", price: 499, availability: "in stock" },
+    { title: "Learn React", author: "Jane Smith", price: 699, availability: "in stock" },
+    { title: "Node.js in Action", author: "Mark Lee", price: 599, availability: "out of stock" },
+    { title: "Python for Everyone", author: "Elison", price: 450, availability: "in stock" },
+    { title: "Head first Java", author: "Elison", price: 1000, availability: "in stock" }
+];
